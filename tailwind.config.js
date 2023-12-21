fd39a4b94f511abc70a221b3bf29f07b6625c3b8
BahasKoding <business.rkhmt@gmail.com>
2023-12-21 21:08:59 +0700

    perubahan tampilan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        dark: '#0f172a',
-        secondary : '#64748b',
+        primary: "#818cf8",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
-      screens:{
-        '2xl': '1320px',
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
-
+};
